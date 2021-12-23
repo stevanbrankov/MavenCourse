0a9b06f1abde4ce6235c5af139dd98211d9ad2b4
jp10 <123steva123@gmail.com>
2021-12-23 20:07:30 +0100

    surefire plugin added

---
+++ MavenPlugins/src/test/java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+public class HelloWorldTest {
+
+    public void testHello(){
+        System.out.println("Test - Hello");
+    }
+
+    public void testWorld(){
+        System.out.println("Test - World");
+    }
+
+}
