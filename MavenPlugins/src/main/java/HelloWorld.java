13fd9e04a378867a199f0686543965ff028fb5c0
jp10 <123steva123@gmail.com>
2021-12-25 17:29:41 +0100

    jsonPlugin added

--- MavenPlugins/src/main/java/HelloWorld.java
+++ MavenPlugins/src/main/java/HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-       // System.out.println(StringUtils.capitalize("hello world"));
     }
 }
